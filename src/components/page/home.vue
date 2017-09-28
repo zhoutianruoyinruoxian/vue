1a2b6e436f3bce4645a204643571578714b44b94
HBZB-PC8\Administrator <961668232@qq.com>
2017-09-28 20:21:26 +0800

    主页画布demo

--- src/components/page/home.vue
+++ src/components/page/home.vue
@@ -12,71 +12,83 @@
 			}
 		},
 		mounted(){ 
-			function  volution(){
+			var el = this.$refs.myCanvas
+			volution(el);
+			function  volution(el){
 
-				const c=this.$refs.myCanvas;
+				const c=el;
 				const ctx=c.getContext("2d");
 				c.width=window.innerWidth;
 				c.height=window.innerHeight-80;
 
 				const setting = {
 					speed: 20,//由于动画周期时间是固定的，所以动画速度由该属性控制(单位:px)
+					time: 0,//此参数放在animate里面用于对时间进行计数
 					pause:false,//是否暂停
-					number：100,//实例数量
-					current：0,//当前实例数量
+					number: 1,//实例数量
+					current: 0,//当前实例数量
 					pack:[],//实例数组
 					colors:[],//颜色数组
 				};
-				const start = function(){//动画开始函数
+				start();
+				function start(){//动画开始函数
 					ctx.clearRect(0, 0, c.width, c.height);
+					setting.current=0;
+					setting.time=0;
 					animate();
 
 				}
-				const Paint =function(X,Y,radius){//每个实例的绘画函数
-					this.dx = X;
-					this.dy = Y;
-					this.radius = radius;
+				function Paint(color){//每个实例的绘画函数
+					this.X = c.width/2;
+					this.Y = c.height/2;
+					this.radius = 10;
+					this.color = color;
 				};
 				Paint.prototype ={
-					paint(){
+					gopaint(){
 						ctx.save();
 				        ctx.beginPath();
-				        ctx.arc( this.dx, this.dy, this.radius , 0, 2 * Math.PI);
+				        ctx.arc( this.X, this.Y, this.radius , 0, 2 * Math.PI);
+						ctx.fillStyle = this.color;
+						ctx.fill();
 						ctx.restore();
 					}
 				}
 
 
-				const animate = function(){//动画执行函数
+				function animate(){//动画执行函数
 					setting.current++;
 					if (setting.current>setting.number) {
-						cancelAnimationFrame(animate);
+						// cancelAnimationFrame(animate);
 					}else{
+						setting.time++;
 						ctx.clearRect(0, 0, c.width, c.height);
-						colors.push(GenerateColor());//生成颜色
+						let new_color = GenerateColor();//生成颜色
 						setting.pack=[];
-						for(let j=0;j<setting.number;j++){//按数量生成实例
-							let ele = new Paint(j,colors);
-							setting.pack.push(ele);
-						};
-						setting.pack.forEach(function(){
-							this.paint();
-						});
-					    if (!setting.pause) {
-					        if ("requestAnimationFrame" in window) {
-					            requestAnimationFrame(animate);
-					        }
-					        else if ("webkitRequestAnimationFrame" in window) {
-					            webkitRequestAnimationFrame(animate);
-					        }
-					        else if ("msRequestAnimationFrame" in window) {
-					            msRequestAnimationFrame(animate);
-					        }
-					        else if ("mozRequestAnimationFrame" in window) {
-					            mozRequestAnimationFrame(animate);
-					        }
-					    };
-					}
+						let ele = new Paint(new_color);//生成实例
+					console.log(ele.gopaint())
+						setting.pack.push(ele);
+					};
+					setting.pack.forEach(function(o){
+						// console.log(o)
+						o.X=Math.cos(Math.PI*setting.speed*setting.time);
+						o.Y=Math.cos(Math.PI*setting.speed*setting.time);
+						o.gopaint();
+					});
+				    if (!setting.pause) {
+				        if ("requestAnimationFrame" in window) {
+				            requestAnimationFrame(animate);
+				        }
+				        else if ("webkitRequestAnimationFrame" in window) {
+				            webkitRequestAnimationFrame(animate);
+				        }
+				        else if ("msRequestAnimationFrame" in window) {
+				            msRequestAnimationFrame(animate);
+				        }
+				        else if ("mozRequestAnimationFrame" in window) {
+				            mozRequestAnimationFrame(animate);
+				        }
+				    };
 				};
 			}
 
@@ -84,7 +96,7 @@
 			// GenerateColor(100,15);
 			function GenerateColor(space){//生成有序的随机颜色
 				let [r,g,b,colors] = [255,0,0,[]];
-				let color= function(){
+				function color(){
 					this.R = r;
 					this.G = g;
 					this.B = b;
