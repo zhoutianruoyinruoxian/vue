ae5057d6e386e86eadb55ffac479b7a53c30653f
HBZB-PC8\Administrator <961668232@qq.com>
2017-09-29 20:17:02 +0800

    初版

--- src/components/page/home.vue
+++ src/components/page/home.vue
@@ -12,69 +12,69 @@
 			}
 		},
 		mounted(){ 
-			var el = this.$refs.myCanvas
+			var el = this.$refs.myCanvas;
 			volution(el);
 			function  volution(el){
+
+				window.onload= function(){
+					start();
+				};
 
 				const c=el;
 				const ctx=c.getContext("2d");
 				c.width=window.innerWidth;
 				c.height=window.innerHeight-80;
-
-				const setting = {
-					speed: 20,//由于动画周期时间是固定的，所以动画速度由该属性控制(单位:px)
+				var setting = {
+					speed: 0.03,//由于动画周期时间是固定的，所以动画速度由该属性控制(单位:px)
 					time: 0,//此参数放在animate里面用于对时间进行计数
 					pause:false,//是否暂停
-					number: 1,//实例数量
+					number: 5,//实例数量
 					current: 0,//当前实例数量
 					pack:[],//实例数组
 					colors:[],//颜色数组
-				};
-				start();
+					space:17,//颜色变化跨度
+				};				
 				function start(){//动画开始函数
 					ctx.clearRect(0, 0, c.width, c.height);
 					setting.current=0;
 					setting.time=0;
+					setting.pack=[];
+					setting.colors =  GenerateColor(setting.space,setting.number);
 					animate();
-
-				}
-				function Paint(color){//每个实例的绘画函数
+				};
+				function Paint(i,color){//每个实例的绘画函数
 					this.X = c.width/2;
 					this.Y = c.height/2;
-					this.radius = 10;
+					this.radius = 4;
 					this.color = color;
+					this.state = i*3;
 				};
-				Paint.prototype ={
-					gopaint(){
+				Paint.prototype.gopaint=function(){
 						ctx.save();
 				        ctx.beginPath();
 				        ctx.arc( this.X, this.Y, this.radius , 0, 2 * Math.PI);
 						ctx.fillStyle = this.color;
+						ctx.globalAlpha=1;
 						ctx.fill();
 						ctx.restore();
-					}
-				}
-
-
+				};
 				function animate(){//动画执行函数
-					setting.current++;
-					if (setting.current>setting.number) {
+					ctx.clearRect(0, 0, c.width, c.height);
+					if (setting.current>=setting.number) {
 						// cancelAnimationFrame(animate);
 					}else{
-						setting.time++;
-						ctx.clearRect(0, 0, c.width, c.height);
-						let new_color = GenerateColor();//生成颜色
-						setting.pack=[];
-						let ele = new Paint(new_color);//生成实例
-					console.log(ele.gopaint())
+						var new_color = setting.colors[setting.time];//获取颜色
+						let ele = new Paint(setting.current,new_color);//生成实例
 						setting.pack.push(ele);
+						setting.current++;
 					};
 					setting.pack.forEach(function(o){
-						// console.log(o)
-						o.X=Math.cos(Math.PI*setting.speed*setting.time);
-						o.Y=Math.cos(Math.PI*setting.speed*setting.time);
+						o.X=Math.cos(Math.PI*setting.speed*(setting.time-o.state))*50+c.width/2;
+						o.Y=Math.sin(Math.PI*setting.speed*(setting.time-o.state))*50+c.width/2;
 						o.gopaint();
 					});
+
+					setting.time++;
 				    if (!setting.pause) {
 				        if ("requestAnimationFrame" in window) {
 				            requestAnimationFrame(animate);
@@ -91,10 +91,15 @@
 				    };
 				};
 			}
-
+			
+			// Array.prototype.forEach = function (callback) {
+			//     for (var i = 0; i < this.length; i++) {
+			//         callback.call(this[i]);
+			//     }
+			// };
 
 			// GenerateColor(100,15);
-			function GenerateColor(space){//生成有序的随机颜色
+			function GenerateColor(space,number){//生成有序的随机颜色
 				let [r,g,b,colors] = [255,0,0,[]];
 				function color(){
 					this.R = r;
@@ -106,25 +111,29 @@
 						return "rgb("+this.R+","+this.G+","+this.B+")"
 					}
 				};
-				if(r==255&&g==0&&b<255){
-					b+=space;
-				}
-				else if (r>0&&g==0&&b==255) {
-					r-=space;
-				}
-				else if(r==0&&g<255&&b==255){
-					g+=space;
-				}
-				else if(r==0&&g==255&&b>0){
-					b-=space;
-				}
-				else if(r<255&&g==255&&b==0){
-					r+=space;
-				}
-				else if(r==255&&g>0&&b==0){
-					g-=space;
+				for(let i=0 ; i<number; i++){
+					if(r==255&&g==0&&b<255){
+						b+=space;
+					}
+					else if (r>0&&g==0&&b==255) {
+						r-=space;
+					}
+					else if(r==0&&g<255&&b==255){
+						g+=space;
+					}
+					else if(r==0&&g==255&&b>0){
+						b-=space;
+					}
+					else if(r<255&&g==255&&b==0){
+						r+=space;
+					}
+					else if(r==255&&g>0&&b==0){
+						g-=space;
+					};
+					let Color= (new color(r,g,b)).getColor();
+					colors.push(Color);
 				};
-				return new color(r,g,b);
+				return colors;
 			};
 		},
 		methods:{
