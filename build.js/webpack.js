ac92f20eefacdb2b7d609c11d790825d7c29d393
HBZB-PC8\Administrator <961668232@qq.com>
2017-10-16 19:58:47 +0800

    webpack配置

--- build.js/webpack.js
+++ build.js/webpack.js
@@ -38,12 +38,25 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),//用于每次构建钱清理/dist文件
-		new HtmlWebpackPlugin({
-			title: '我的VUE'
-		}),
 		new ExtractTextPlugin({//分离css文件
-	      	filename: '/static/css/[name].css'
-	    }),
+			filename: '/static/css/[name].css'
+		}),
+		new HtmlWebpackPlugin({//插件地址https://github.com/jantimon/html-webpack-plugin#configuration
+			title: '我的VUE',
+			filename: 'index.html',
+			template: 'index.html',
+			favicon: path.resolve(__dirname, './src/assets/logo.png'),//将给定的图标路径添加到输出html
+			inject: true,//将true | 'head' | 'body' | false所有资产注入给定的template或templateContent- 当通过true或'body'所有JavaScript资源将被放置在body元素的底部时。'head'将脚本放在头元素中。
+			minify: {//输出最小化
+				removeComments: true,//剥离HTML注释
+				collapseWhitespace: true,//折叠对文档树中的文本节点有贡献的空白空间
+				removeAttributeQuotes: true//尽可能删除属性的引号
+				// more options:
+				// https://github.com/kangax/html-minifier#options-quick-reference
+			},
+			// necessary to consistently work with multiple chunks via CommonsChunkPlugin
+			chunksSortMode: 'dependency'
+		}),
 	],
 	resolve: {//模块路径
 		extensions:['.js','.jsx','.vue','.json'],//此选项告诉解析器在解析中能够接受哪些扩展名
